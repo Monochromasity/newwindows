7153f27abe7df47062ad8b5b9b4b10fe84a5f92e
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2025-04-25 19:50:24 +0100

    v1.1 - changed open to use eval to be accessible from external, added close

--- [UNSANDBOXED] extension.js
+++ [UNSANDBOXED] extension.js
@@ -13,9 +13,9 @@
         name: 'New Windows',
         blocks: [
           {
-            opcode: 'openwebsite',
+            opcode: 'openwindow',
             blockType: Scratch.BlockType.COMMAND,
-            text: 'open website [WEB] in new window with ID [ID], width [WIDTH], and height [HEIGHT]',
+            text: 'open website [WEB] in new window with ID [ID] width [WIDTH] and height [HEIGHT]',
             arguments: {
               WEB: {
                 type: Scratch.ArgumentType.STRING,
@@ -34,6 +34,17 @@
                 defaultValue: '400'
               }
             }
+          },
+          {
+            opcode: 'closewithid',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'close window with id [ID]',
+            arguments: {
+              ID: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'myWindow'
+              }
+            }
           }
         ]
       };
@@ -46,8 +57,11 @@
       let height = args.HEIGHT;
       let strstart = "width=";
       let str = strstart.concat(width, ", height=", height);
-      window[id] = window.open(web, "", str);
+      eval("newwindows-" + id + " = window.open(web, '', str);");
     }
+    closewithid(args) {
+      let id = args.ID;
+      eval("newwindows-" + id + ".close();");
   }
   Scratch.extensions.register(new NewWindows());
 })(Scratch);
