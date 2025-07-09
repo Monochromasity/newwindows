d9b5fcc4bfedf7a8faa63fcb40b26c262a5d616b
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2025-07-09 15:19:32 +0100

    Update [UNSANDBOXED] extension.js

--- [UNSANDBOXED] extension.js
+++ [UNSANDBOXED] extension.js
@@ -15,7 +15,7 @@
           {
             opcode: 'openwindow',
             blockType: Scratch.BlockType.COMMAND,
-            text: 'open website [WEB] in new window with ID [ID] width [WIDTH] and height [HEIGHT]',
+            text: 'open website [WEB] in new window with ID: [ID] width: [WIDTH] height: [HEIGHT] distance from left: [LEFT] distance from top: [TOP] fullscreen (Internet Explorer): [FULLSCREEN]',
             arguments: {
               WEB: {
                 type: Scratch.ArgumentType.STRING,
@@ -32,7 +32,20 @@
               HEIGHT: {
                 type: Scratch.ArgumentType.NUMBER,
                 defaultValue: '400'
-              }
+              },
+              LEFT: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '0'
+              },
+              TOP: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '0'
+              },
+              FULLSCREEN: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'FULLSCREEN',
+                defaultValue: 'no'
+              },
             }
           },
           {
@@ -46,7 +59,13 @@
               }
             }
           }
-        ]
+        ],
+        menus: {
+          FULLSCREEN: {
+            acceptReporters: true,
+            items: ['yes', 'no']
+          }
+        }
       };
     }
 
@@ -57,7 +76,10 @@
       let width = args.WIDTH;
       let height = args.HEIGHT;
       let strstart = "width=";
-      let str = strstart.concat(width, ", height=", height);
+      let full = args.FULLSCREEN;
+      let left = Math.abs(Number(args.LEFT));
+      let top = Math.abs(Number(args.TOP));
+      let str = strstart.concat(width, ", height=", height, "fullscreen=", full, "left=", left, "top=", top);
       let name = "windows" + id
       this[name] = window.open(web, '', str);
     }
