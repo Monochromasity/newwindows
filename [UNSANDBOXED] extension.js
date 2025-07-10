a058473209a86f526ed3f821067c8dec43388972
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2025-07-10 14:26:43 +0100

    LOTS MORE STUFF :D

--- [UNSANDBOXED] extension.js
+++ [UNSANDBOXED] extension.js
@@ -15,7 +15,7 @@
           {
             opcode: 'openwindow',
             blockType: Scratch.BlockType.COMMAND,
-            text: 'open website [WEB] in new window with ID: [ID] width: [WIDTH] height: [HEIGHT] distance from left: [LEFT] distance from top: [TOP] fullscreen (Internet Explorer): [FULLSCREEN]',
+            text: 'open website [WEB] in new window with ID: [ID] width: [WIDTH] height: [HEIGHT] distance from left: [LEFT] distance from top: [TOP] show menu bar: [MENUBAR] add status bar: [STATUSBAR] show title bar: [TITLEBAR] fullscreen (Internet Explorer): [FULLSCREEN] show address field (Opera): [LOCATION] resizable (Internet Explorer): [RESIZABLE] show scrollbars (Internet Explorer, Firefox, Opera): [SCROLL] show toolbar (Internet Explorer, Firefox): [TOOLBAR] replace (Deprecated): [REPLACE]',
             arguments: {
               WEB: {
                 type: Scratch.ArgumentType.STRING,
@@ -41,11 +41,51 @@
                 type: Scratch.ArgumentType.NUMBER,
                 defaultValue: '0'
               },
+              MENUBAR: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'MENUBAR',
+                defaultValue: 'no'
+              },
+              STATUSBAR: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'STATUSBAR',
+                defaultValue: 'no'
+              },
+              TITLEBAR: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'TITLEBAR',
+                defaultValue: 'no'
+              },
               FULLSCREEN: {
                 type: Scratch.ArgumentType.STRING,
                 menu: 'FULLSCREEN',
                 defaultValue: 'no'
               },
+              LOCATION: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'LOCATION',
+                defaultValue: 'no'
+              },
+              RESIZABLE: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'RESIZABLE',
+                defaultValue: 'yes'
+              },
+              SCROLL: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'SCROLL',
+                defaultValue: 'yes'
+              },
+              TOOLBAR: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'TOOLBAR',
+                defaultValue: 'yes'
+              },
+              REPLACE: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'REPLACE',
+                defaultValue: 'false'
+              }
             }
           },
           {
@@ -64,6 +104,38 @@
           FULLSCREEN: {
             acceptReporters: true,
             items: ['yes', 'no']
+          },
+          MENUBAR: {
+            acceptReporters: true,
+            items: ['yes', 'no']
+          },
+          STATUSBAR: {
+            acceptReporters: true,
+            items: ['yes', 'no']
+          },
+          TITLEBAR: {
+            acceptReporters: true,
+            items: ['yes', 'no']
+          },
+          LOCATION: {
+            acceptReporters: true,
+            items: ['yes', 'no']
+          },
+          RESIZABLE: {
+            acceptReporters: true,
+            items: ['yes', 'no']
+          },
+          SCROLL: {
+            acceptReporters: true,
+            items: ['yes', 'no']
+          },
+          TOOLBAR: {
+            acceptReporters: true,
+            items: ['yes', 'no']
+          },
+          REPLACE: {
+            acceptReporters: true,
+            items: ['true', 'false']
           }
         }
       };
@@ -79,9 +151,17 @@
       let full = args.FULLSCREEN;
       let left = Math.abs(Number(args.LEFT));
       let top = Math.abs(Number(args.TOP));
-      let str = strstart.concat(width, ", height=", height, "fullscreen=", full, "left=", left, "top=", top);
-      let name = "windows" + id
-      this[name] = window.open(web, '', str);
+      let menubar = args.MENUBAR;
+      let status = args.STATUSBAR;
+      let titlebar = args.TITLEBAR;
+      let location = args.LOCATION;
+      let resizable = args.RESIZABLE;
+      let scroll = args.SCROLL;
+      let toolbar = args.TOOLBAR;
+      let replace = args.REPLACE;
+      let str = strstart.concat(width, ",height=", height, ",fullscreen=", full, ",left=", left, ",top=", top, ",menubar=", menubar, ",status=", status, ",titlebar=", titlebar, ",location=", location, ",resizable=", resizable, ",scrollbars=", scroll, ",toolbar=", toolbar);
+      let name = "windows" + id;
+      this[name] = window.open(web, '', str, replace);
     }
     closewithid(args) {
       let iduf = args.ID;
